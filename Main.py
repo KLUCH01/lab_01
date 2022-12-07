1397eab302edc0d8abd880293dcb6befbfa46137
Данил Ключников <cliuchnikov.danil@yandex.ru>
2022-12-07 15:24:07 +0500

    Создаем новый коммит 1!!!

--- Main.py
+++ Main.py
@@ -7,11 +7,17 @@ Mas = []
 
 i = 0
 for i in range(N):
-if (i==0): Mas.append(1)
-if (i==1): Mas.append(1)
-if (i>1): Mas.append(Mas[i-1] + Mas[i-2])
+    if (i==0): Mas.append(1)
+    if (i==1): Mas.append(1)
+    if (i>1): Mas.append(Mas[i-1] + Mas[i-2])
+
 print(Mas)
 Y = []
 for i in range(N): Y.append(i)
+
+
+
+
+
 matplotlib.pyplot.plot(Y, Mas)
 matplotlib.pyplot.show()
